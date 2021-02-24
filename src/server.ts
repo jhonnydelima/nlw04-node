70b24b631d49c46b45c59788672f3720187496ec
Jhonny Lima <jlima@Jhonnys-MacBook-Pro.local>
2021-02-24 16:37:47 -0300

    added database to the project

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
+import 'reflect-metadata';
+import './database';
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.json({ message: "Hello World - NLW04" })
-});
-
-app.post("/", (req, res) => {
-    return res.json({ message: "Os dados foram salvos com sucesso!" })
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => {
     console.log("Server is running!")
